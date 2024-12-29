faa616bbf7a1a8b1113832043932868e3866c677
Athaya Laily Syafitri <144782069+athayalailys@users.noreply.github.com>
2024-12-29 22:47:53 +0800

    Create Soal5.java

---
+++ modul1/Soal5.java
@@ -0,0 +1,23 @@
+package praktikum1;
+
+import java.util.Scanner;
+
+public class Soal5 {
+    static final double PHI = 3.14;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan jari-jari: ");
+        double jariJari = scanner.nextDouble();
+
+        System.out.print("Masukkan tinggi: ");
+        double tinggi = scanner.nextDouble();
+
+        double volume = PHI * (jariJari * jariJari) * tinggi;
+
+        System.out.printf("Volume tabung dengan jari-jari %.1f cm dan tinggi %.1f cm adalah %.3f m³%n", jariJari, tinggi, volume);
+        
+        scanner.close();
+    }
+}
